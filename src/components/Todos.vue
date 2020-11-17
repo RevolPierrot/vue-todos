9e96406453d25d77966df30e170adffbf8b2ba56
RevolPierrot <70239446+RevolPierrot@users.noreply.github.com>
2020-11-17 10:38:41 +0100

    -added checkbox and button incl. styles

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,7 +2,13 @@
     <div>
         <h3>My Todos</h3>
         <ul>
-            <li :key="item.id" v-for="item in todos">{{item.text}}</li>
+            <li :key="item.id" v-for="item in todos">
+                <input type="checkbox" v-if="item.done" checked/>
+                <input type="checkbox" v-else/>
+                <span v-if="item.done" class="done">{{item.text}}</span>
+                <span v-else>{{item.text}}</span>
+                <button class="button-del">Löschen</button>
+            </li>
         </ul>
     </div>
 </template>
@@ -44,7 +50,21 @@ ul {
     margin: auto;
 }
 
+.button-del {
+    float: right;
+}
+
 li {
     list-style-type: none;
+    margin: 15px;
+}
+
+li span {
+    margin-left: 10px;
+}
+
+.done {
+    text-decoration: line-through;
+    color: #0a0;
 }
 </style>
